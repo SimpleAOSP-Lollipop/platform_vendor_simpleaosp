934ac5ee13e51c95e4bff05475fb03589408ecfd
Clark Scheff <clark@cyngn.com>
2014-04-28 12:03:07 -0700

    Themes: Add feature for new themes.
    Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3
    
    Conflicts:
    	configs/themes_common.mk

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -5,6 +5,5 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
-
-
+       vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/simpleaosp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
