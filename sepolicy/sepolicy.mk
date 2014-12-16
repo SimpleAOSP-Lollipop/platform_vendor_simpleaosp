1029b9c5765f6718b7174da1ad762dc49415705c
Andy Mast <andy@cyngn.com>
2014-12-16 11:05:54 -0800

    selinux: New rw privileges for themes
    - New theme_data_file context for files under /data/system/theme
    - Permit systemserver to create files/dirs under /data/resource-cache
    - Permit systemserver to create files/dirs under /data/system/theme
    
    Change-Id: Id597fc20b477ea395a8631623f26a7edde280799
    
    Conflicts:
    	sepolicy/file.te
    	sepolicy/file_contexts
    	sepolicy/system.te

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -6,6 +6,9 @@ BOARD_SEPOLICY_DIRS += \
     vendor/simpleaosp/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    system.te \
     app.te \
     service_contexts \
     bootanim.te \
