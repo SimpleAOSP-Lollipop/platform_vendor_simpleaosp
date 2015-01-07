05318cef3ec4e265460159d6862dce2bb09ff31e
MrBaNkS <banksmi09@gmail.com>
2015-01-07 10:18:07 -0600

    Enable tethering for all devices

--- configs/common.mk
+++ configs/common.mk
@@ -25,6 +25,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.lively_timeout=2500 \
     ro.facelock.est_max_time=600 \
     ro.facelock.use_intro_anim=false \
+    net.tethering.noprovisioning=true \
     ro.delta.version=VERSION
 
 # Common overlays
