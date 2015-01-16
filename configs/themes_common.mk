391559ec247e08fb49d7d267d52cf8cbfc71ac1f
snak3ater <saeedyasir91@gmail.com>
2015-01-16 23:43:14 +0500

    build new theme chooser

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,8 +1,7 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
        vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
