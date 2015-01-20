CyanogenMod/android_vendor_cm

Add Theme Engine to the builds
Change-Id: I57568ebef3f9800e0ad7c39b6352186470ad9f08

config/common.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
+# T-Mobile theme engine
+include vendor/cm/config/themes_common.mk
+
 # Required CM packages
 PRODUCT_PACKAGES += \
     Camera \

config/themes_common.mk
@@ -1,0 +1,8 @@
+# T-Mobile theme engine
+PRODUCT_PACKAGES += \
+       ThemeManager \
+       ThemeChooser \
+       com.tmobile.themes
+
+PRODUCT_COPY_FILES += \
+       vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml

SimpleAOSP-Lollipop/platform_vendor_simpleaosp

configs/common.mk
@@ -65,3 +65,6 @@ PRODUCT_COPY_FILES += \
 
 # Include bootanimation mk file
 -include vendor/simpleaosp/configs/bootanimation.mk
+
+# T-Mobile theme engine
+ include vendor/simpleaosp/configs/themes_common.mk

configs/themes_common.mk
@@ -1,0 +1,10 @@
+# T-Mobile theme engine
+PRODUCT_PACKAGES += \
+       ThemeManager \
+       ThemeChooser \
+       com.tmobile.themes
+
+PRODUCT_COPY_FILES += \
+       vendor/simpleaosp/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+
+